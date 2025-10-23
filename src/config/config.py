33ba590bbd0e5a1a92e7e3072960ae2c2bd052ff
Ana <anaraquelysmachado29@gmail.com>
2025-10-23 02:26:55 -0300

    API-80-feat:add configuration file for environment variables

---
+++ src/config/config.py
@@ -0,0 +1,9 @@
+import os
+from flask.cli import load_dotenv
+
+load_dotenv()
+
+CSV_PATH = os.getenv("CSV_PATH")
+DATE_COL = os.getenv("DATE_COL")
+COMPANY_COL = os.getenv("COMPANY_COL")
+METRICS_CSV = os.getenv("METRICS_CSV")
